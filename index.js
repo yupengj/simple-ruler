1178cc40a190b10a2fc3e2b8f1ced583ad0e951a
jyp10@qq.com <jyp10@qq.com>
2018-06-30 00:23:26 +0800

    u

--- index.js
+++ index.js
@@ -33,20 +33,30 @@ Page({
   },
   // 计算一毫米等于多少px，然后以一毫米的px递增
   onGetOnecmPxiel: function (res) {
-    var num = this.data[res.model];
+    var modal = res.model;
+    if (modal.indexOf("<") != -1){
+      modal = modal.substring(0, modal.indexOf("<"))
+    }
+    var num = this.data[modal];
     if (!num){
       num = 5.2;
     }
     return num;
   },
   data: {
-    "iPhone 5": 5.4,
-    "iPhone 6": 5.8,
-    "iPhone 6 Plus": 5.9,
+    "iPhone 5": 6.4,
+    "iPhone SE": 6.4,// v
+    "iPhone 6": 6.0,
+    "iPhone 6 Plus": 6.1,
+    "iPhone 6s": 6.0,
+    "iPhone 6s Plus": 6.1,// v
     "iPhone 7": 6.0,
     "iPhone 7 Plus": 6.1,
     "iPhone 8": 6.0,
     "iPhone 8 Plus": 6.1,
-    "OD105": 5.3
+    "OD105": 5.3,// 坚果pro v
+    "OS105": 6.36,// 坚果pro2 v
+    "SM-G9500":5.65, // 三星 95v v
+    "BKL-AL20":5.34 // 荣耀V10 v
   }
 });
