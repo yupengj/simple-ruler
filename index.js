0adff370fc3170193119fa5040360d3a4559bcca
姜玉鹏 <jiangyp@gantsoftware.com>
2023-12-01 23:11:29 +0800

    v0.3

--- index.js
+++ index.js
@@ -8,27 +8,22 @@ Page({
       const context = canvas.getContext('2d')
 
       var window = wx.getSystemInfoSync();
-      console.log(window)
-      var ppi = this.estimatePPI(window)
       var dpr = window.pixelRatio
-      var mmOfCount = (ppi/25.4).toFixed(3)// 1 毫米有几个像素
-      console.log(ppi+"--"+dpr+"--"+mmOfCount)
-
-      // 设置Canvas的实际大小
+      var maxWidth = window.windowWidth; 
+      var maxHeight = window.screenHeight; 
+      var onemm = 5; 
+   
       canvas.width = result[0].width * dpr;
       canvas.height = result[0].height * dpr;
       context.scale(dpr, dpr);
 
-      var th = 20*mmOfCount/dpr;
-      context.moveTo(10, 10)
-      context.lineTo(10, 10+th)
-      context.stroke();
+      // var ppi = this.estimatePPI(window) // 1英寸有几个像素
+      // var mmOfCount = (ppi/25.4).toFixed(3)// 1 毫米有几个像素
+      // console.log(ppi+"--"+dpr+"--"+mmOfCount)
 
-      // context.scale(dpr, dpr); // 缩放Canvas
-
-      // context.font = "20px serif";
-      // this.onDrawRuler(context, 10, maxHeight, 5, 0, 10, true);
-      // this.onDrawRuler(context, 10, maxHeight, 5, maxWidth - 10, maxWidth, false);
+      context.font = "20px serif";
+      this.onDrawRuler(context, 10, maxHeight, onemm, 0, 10, true);
+      this.onDrawRuler(context, 10, maxHeight, onemm, maxWidth - 10, maxWidth, false);
     });
   },
   onDrawRuler: function (context, start, end, onemm, lineStart, lineEnd, isLeft) {
@@ -65,7 +60,6 @@ Page({
     console.log("screenInches",screenInches)
     // 估算PPI
     var estimatedPPI = Math.sqrt(screenWidth ** 2 + screenHeight ** 2) / screenInches;
-
     return estimatedPPI;
   },
 });
